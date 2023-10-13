a095a3529caddc5a7008b47e53cb65b3ae4b4754
Cedric Vogel <cedric_joerg.vogel@mailbox.tu-dresden.de>
2023-10-13 18:38:58 +0200

    add email field to entries

--- src/main/java/guestbook/GuestbookEntry.java
+++ src/main/java/guestbook/GuestbookEntry.java
@@ -34,20 +34,22 @@ import org.springframework.util.Assert;
 class GuestbookEntry {
 
 	private @Id @GeneratedValue Long id;
-	private final String name, text;
+	private final String name, text, email;
 	private final LocalDateTime date;
-
 	/**
 	 * Creates a new {@link GuestbookEntry} for the given name and text.
 	 *
-	 * @param name must not be {@literal null} or empty
-	 * @param text must not be {@literal null} or empty
+	 * @param name  must not be {@literal null} or empty
+	 * @param text  must not be {@literal null} or empty
+	 * @param email must not be {@literal null} or empty
 	 */
-	public GuestbookEntry(String name, String text) {
+	public GuestbookEntry(String name, String text, String email) {
 
 		Assert.hasText(name, "Name must not be null or empty!");
 		Assert.hasText(text, "Text must not be null or empty!");
+		Assert.hasText(email, "E-Mail must not be null or empty!");
 
+		this.email = email;
 		this.name = name;
 		this.text = text;
 		this.date = LocalDateTime.now();
@@ -58,6 +60,7 @@ class GuestbookEntry {
 		this.name = null;
 		this.text = null;
 		this.date = null;
+		this.email = null;
 	}
 
 	public String getName() {
@@ -75,4 +78,6 @@ class GuestbookEntry {
 	public String getText() {
 		return text;
 	}
+
+	public String getEmail(){return email;}
 }
